84140ab77f6a1317425857745c6b26167c098919
Malits <Malits@Malits.com>
2020-02-20 01:59:27 +0800

    add icon

--- app/src/main/java/com/github/malitsplus/shizurunotes/ui/MainActivity.kt
+++ app/src/main/java/com/github/malitsplus/shizurunotes/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
