d31feaad75a3709541168403f69c637341fe0ee2
Vibbit <hua8102259@gmail.com>
2020-03-30 17:30:09 +0800

    edit

--- app/src/main/java/com/github/malitsplus/shizurunotes/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/github/malitsplus/shizurunotes/ui/calendar/CalendarFragment.kt
@@ -2,17 +2,18 @@ package com.github.malitsplus.shizurunotes.ui.calendar
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.provider.CalendarContract.Calendars
-import androidx.fragment.app.Fragment
+import android.provider.CalendarContract.Events
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.github.malitsplus.shizurunotes.R
-import java.lang.StringBuilder
+
 
 class CalendarFragment : Fragment() {
 
@@ -88,6 +89,7 @@ class CalendarFragment : Fragment() {
         }
         sb.toString()
 
+
     }
 
     override fun onCreateView(
@@ -95,7 +97,67 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        val uri = CalendarContract.Events.CONTENT_URI as Uri
+        val projection = CALENDAR_PROJECTION
+        val selection = "( ${CalendarContract.Events.CALENDAR_ID} = ?)"
+        val selectionArgs = arrayOf(2)
+        val sortOrder = null
+        val contentResolver = requireContext().contentResolver
+        val cursor = if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_CALENDAR
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            throw IllegalAccessException("No permission to Calendar.")
+            //return
+        } else {
+            contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)
+        }
+
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
+
+    private val CALENDAR_PROJECTION = arrayOf(
+        Calendars._ID,
+        Calendars.NAME,
+        Calendars.ACCOUNT_NAME,
+        Calendars.ACCOUNT_TYPE,
+        Calendars.CALENDAR_COLOR,
+        Calendars.CALENDAR_DISPLAY_NAME,
+        Calendars.CALENDAR_ACCESS_LEVEL,
+        Calendars.CALENDAR_TIME_ZONE,
+        Calendars.VISIBLE,
+        Calendars.SYNC_EVENTS,
+        Calendars.OWNER_ACCOUNT
+    )
+
+    private val EVENT_PROJECTION = arrayOf(
+        Events.CALENDAR_ID,
+        Events.TITLE,
+        Events.DESCRIPTION,
+        Events.EVENT_LOCATION,
+        Events.EVENT_COLOR,
+        Events.DISPLAY_COLOR,
+        Events.DTSTART,
+        Events.DTEND,
+        Events.DURATION,
+        Events.EVENT_TIMEZONE,
+        Events.EVENT_END_TIMEZONE,
+        Events.ALL_DAY,
+        Events.RRULE,
+        Events.RDATE,
+        Events.GUESTS_CAN_MODIFY,
+        Events.GUESTS_CAN_INVITE_OTHERS,
+        Events.GUESTS_CAN_SEE_GUESTS,
+        Events.ORGANIZER
+    )
 
 }
