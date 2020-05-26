43bd4b963179175bcdf121ba22245d03e7f7beeb
Vibbit <hua8102259@gmail.com>
2020-05-26 17:31:53 +0800

    fix some bugs

--- app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ChangeEnergyRecoveryRatioByDamageAction.java
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ChangeEnergyRecoveryRatioByDamageAction.java
@@ -3,16 +3,30 @@ package com.github.malitsplus.shizurunotes.data.action;
 import com.github.malitsplus.shizurunotes.R;
 import com.github.malitsplus.shizurunotes.common.I18N;
 import com.github.malitsplus.shizurunotes.data.Property;
+import com.github.malitsplus.shizurunotes.data.Skill;
 
 public class ChangeEnergyRecoveryRatioByDamageAction extends ActionParameter {
+
     @Override
     protected void childInit() {
         super.childInit();
     }
 
+    protected String getChildrenActionString() {
+        StringBuilder childrenActionString = new StringBuilder();
+        if (childrenAction != null) {
+            for (Skill.Action action : childrenAction) {
+                childrenActionString.append(action.getActionId() % 10).append(", ");
+            }
+            childrenActionString.delete(childrenActionString.lastIndexOf(", "), childrenActionString.length());
+        }
+        return childrenActionString.toString();
+    }
+
     @Override
     public String localizedDetail(int level, Property property) {
-        return I18N.getString(R.string.change_energy_recovery_ratio_by_damage_to_s1_when_s2_get_damage_next_time,
+        return I18N.getString(R.string.change_energy_recovery_ratio_of_action_s1_to_s2_when_s3_get_damage,
+                getChildrenActionString(),
                 actionValue1,
                 targetParameter.buildTargetClause());
     }
