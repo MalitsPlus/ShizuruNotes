ce0c518f0ed4fa32ca5ee66da5c06a924b2bb1f1
Vibbit <hua8102259@gmail.com>
2020-09-21 17:29:24 +0800

    update skills improve ui copy homework

--- app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ChangeParameterFieldAction.java
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ChangeParameterFieldAction.java
@@ -8,8 +8,6 @@ import java.math.RoundingMode;
 
 public class ChangeParameterFieldAction extends AuraAction {
 
-    protected String percentModifier = "";
-
     @Override
     protected void childInit() {
         super.childInit();
@@ -17,9 +15,7 @@ public class ChangeParameterFieldAction extends AuraAction {
         actionValues.add(new ActionValue(actionValue1, actionValue2, null));
         durationValues.clear();
         durationValues.add(new ActionValue(actionValue3, actionValue4, null));
-        if (auraType == AuraType.physicalCriticalDamage || auraType == AuraType.magicalCriticalDamage) {
-            percentModifier = "%";
-        }
+        super.percentModifier = PercentModifier.parse(actionDetail2);
     }
 
     @Override
@@ -32,7 +28,7 @@ public class ChangeParameterFieldAction extends AuraAction {
                     buildExpression(level, RoundingMode.UP, property),
                     auraType.description(),
                     buildExpression(level, durationValues, RoundingMode.UNNECESSARY, property),
-                    percentModifier);
+                    percentModifier.description());
         } else {
             return I18N.getString(R.string.Summon_a_field_of_radius_d1_at_position_of_s2_to_s3_s4_s5_for_s6_sec,
                     (int)actionValue5,
@@ -41,7 +37,7 @@ public class ChangeParameterFieldAction extends AuraAction {
                     buildExpression(level, RoundingMode.UP, property),
                     auraType.description(),
                     buildExpression(level, durationValues, RoundingMode.UNNECESSARY, property),
-                    percentModifier);
+                    percentModifier.description());
         }
     }
 }
--- app/src/main/java/com/github/malitsplus/shizurunotes/data/action/TargetParameter.java
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/TargetParameter.java
@@ -304,7 +304,10 @@ enum TargetType{
     magicSTRDescendingOrNear(31),
     magicSTRAscendingOrNear(32),
     shadow(33),
-    nearWithoutSelf(34);
+    nearWithoutSelf(34),
+    hpDescendingOrNearForward(35),
+    hpAscendingOrNearForward(36),
+    tpDescendingOrMaxForward(37);
 
     private int value;
     TargetType(int value){
@@ -373,6 +376,10 @@ enum TargetType{
             case hpAscending:
             case hpAscendingOrNear:
                 return I18N.getString(R.string.the_lowest_HP_ratio);
+            case hpAscendingOrNearForward:
+                return I18N.getString(R.string.the_lowest_HP);
+            case hpDescendingOrNearForward:
+                return I18N.getString(R.string.the_highest_HP);
             case hpDescending:
             case hpDescendingOrNear:
                 return I18N.getString(R.string.the_highest_HP_ratio);
@@ -386,6 +393,7 @@ enum TargetType{
                 return I18N.getString(R.string.targets_within_the_scope);
             case tpDescending:
             case tpDescendingOrNear:
+            case tpDescendingOrMaxForward:
                 return I18N.getString(R.string.the_highest_TP);
             case tpAscending:
             case tpReducing:
@@ -439,12 +447,17 @@ enum TargetType{
                 return I18N.getString(R.string.s_lowest_HP_ratio, localizedModifier);
             case hpDescending:
                 return I18N.getString(R.string.s_highest_HP_ratio, localizedModifier);
+            case hpAscendingOrNearForward:
+                return I18N.getString(R.string.s_lowest_HP, localizedModifier);
+            case hpDescendingOrNearForward:
+                return I18N.getString(R.string.s_highest_HP, localizedModifier);
             case forward:
                 return I18N.getString(R.string.s_most_backward, localizedModifier);
             case backward:
                 return I18N.getString(R.string.s_most_forward, localizedModifier);
             case tpDescending:
             case tpDescendingOrNear:
+            case tpDescendingOrMaxForward:
                 return I18N.getString(R.string.s_highest_TP, localizedModifier);
             case tpAscending:
             case tpReducing:
@@ -505,6 +518,10 @@ enum TargetType{
                 case hpDescending:
                 case hpDescendingOrNear:
                     return I18N.getString(R.string.the_s_highest_HP_ratio, localizedModifier);
+                case hpAscendingOrNearForward:
+                    return I18N.getString(R.string.the_s_lowest_HP, localizedModifier);
+                case hpDescendingOrNearForward:
+                    return I18N.getString(R.string.the_s_highest_HP, localizedModifier);
                 case forward:
                     return I18N.getString(R.string.the_s_most_backward, localizedModifier);
                 case backward:
