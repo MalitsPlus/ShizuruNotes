a88f5e4e764bed12cd0b85a3e738c2bdc654c766
Vibbit <hua8102259@gmail.com>
2020-11-30 15:13:45 +0800

    fix target description #35

--- app/src/main/java/com/github/malitsplus/shizurunotes/data/action/TargetParameter.java
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/TargetParameter.java
@@ -36,11 +36,7 @@ public class TargetParameter {
     private boolean hasTargetType;
     private boolean hasDependAction(){
         return dependAction != null && (dependAction.getActionId() != 0
-                && targetType != TargetType.absolute
-                && (ActionType.ifForChildren == dependAction.parameter.actionType
-                || ActionType.ifForAll == dependAction.parameter.actionType
-                || ActionType.damage == dependAction.parameter.actionType
-                || ActionType.knock == dependAction.parameter.actionType));
+                && targetType != TargetType.absolute);
     }
 
     private void setBooleans(){
