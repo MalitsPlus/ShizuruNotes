52cc097f7d847deaf6ceba868ae0bea48bf883cc
MalitsPlus <hua8102259@gmail.com>
2021-12-25 13:45:15 +0800

    Added new skill descriptions

---
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/EffectAction.java
@@ -0,0 +1,18 @@
+package com.github.malitsplus.shizurunotes.data.action;
+
+import com.github.malitsplus.shizurunotes.R;
+import com.github.malitsplus.shizurunotes.common.I18N;
+import com.github.malitsplus.shizurunotes.data.Property;
+
+public class EffectAction extends ActionParameter{
+
+    @Override
+    protected void childInit() {
+        super.childInit();
+    }
+
+    @Override
+    public String localizedDetail(int level, Property property) {
+        return I18N.getString(R.string.Implement_some_visual_effects_to_s1, targetParameter.buildTargetClause());
+    }
+}
