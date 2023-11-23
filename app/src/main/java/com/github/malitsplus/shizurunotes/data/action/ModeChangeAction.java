840ec2ded072b33715a8b00492f6a69aeb581f64
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2023-11-23 12:53:55 +0900

    fix: flight status #87

--- app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ModeChangeAction.java
+++ app/src/main/java/com/github/malitsplus/shizurunotes/data/action/ModeChangeAction.java
@@ -30,25 +30,66 @@ public class ModeChangeAction extends ActionParameter {
         }
     }
 
-    private ModeChangeType modeChangeType;
+    enum AdditionalAbnormalType{
+        none(0),
+        flight(1);
+
+        private int value;
+        AdditionalAbnormalType(int value){
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
+
+        public static AdditionalAbnormalType parse(int value){
+            for(AdditionalAbnormalType item : AdditionalAbnormalType.values()){
+                if(item.getValue() == value)
+                    return item;
+            }
+            return none;
+        }
+
+        public String description(){
+            switch (this){
+                case flight: return I18N.getString(R.string.flight);
+                default: return I18N.getString(R.string.none);
+            }
+        }
+    }
+
+    protected ModeChangeType modeChangeType;
+    protected AdditionalAbnormalType abnormalType;
 
     @Override
     protected void childInit() {
         modeChangeType = ModeChangeType.parse(actionDetail1);
+        abnormalType = AdditionalAbnormalType.parse(Integer.parseInt(actionValue5.valueString()));
     }
 
     @Override
     public String localizedDetail(int level, Property property) {
+        String additionalDescription =  abnormalType == AdditionalAbnormalType.none
+            ? ""
+            : I18N.getString(R.string.Applies_s1_status_to_self, abnormalType.description());
         switch (modeChangeType){
             case time:
-                return I18N.getString(R.string.Change_attack_pattern_to_d1_for_s2_sec,
-                        actionDetail2 % 10, actionValue1.valueString());
+                return I18N.getString(
+                    R.string.Change_attack_pattern_to_d1_for_s2_sec,
+                    actionDetail2 % 10,
+                    actionValue1.valueString()
+                ) + additionalDescription;
             case energy:
-                return I18N.getString(R.string.Cost_s1_TP_sec_change_attack_pattern_to_d2_until_TP_is_zero,
-                        Utils.roundDownDouble(actionValue1.value), actionDetail2 % 10);
+                return I18N.getString(
+                    R.string.Cost_s1_TP_sec_change_attack_pattern_to_d2_until_TP_is_zero,
+                    Utils.roundDownDouble(actionValue1.value),
+                    actionDetail2 % 10
+                ) + additionalDescription;
             case release:
-                return I18N.getString(R.string.Change_attack_pattern_back_to_d_after_effect_over,
-                        actionDetail2 % 10);
+                return I18N.getString(
+                    R.string.Change_attack_pattern_back_to_d_after_effect_over,
+                    actionDetail2 % 10
+                ) + additionalDescription;
             default:
                 return super.localizedDetail(level, property);
         }
